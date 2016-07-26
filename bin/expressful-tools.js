3f70e2540511dbafa25ec49129409d5136db08c5
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-26 16:10:47 -0700

    removed old script

--- bin/expressful-tools.js
+++ bin/expressful-tools.js
@@ -16,6 +16,6 @@ case 'eject':
   break;
 default:
   console.log('Unknown script "' + script + '".');
-  console.log('Perhaps you need to update react-scripts?');
+  console.log('Perhaps you need to update expressful-tools?');
   break;
 }
