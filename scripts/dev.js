8d275d87b1bb516b391ebaab19b1f300e338fedf
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-27 20:18:07 -0700

    css path fix

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,6 +3,9 @@ var settings = require('../config/settings');
 var watchCss = require('./tasks/watch-css');
 var serve = require('./tasks/serve');
 
+// Do a build before starting
+require('./build');
+
 function dev () {
   watchCss(settings);
   serve(settings);
