50a3eb4e0cb83f7c1c000dda5601fffb0c6b7d69
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-28 19:48:53 -0700

    js build fix

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,10 @@
 'use strict';
 var settings = require('../config/settings');
 var buildCss = require('./tasks/build-css');
+var buildJavascript = require('./tasks/build-javascript');
 
 function build () {
+  buildJavascript(settings);
   buildCss(settings);
 }
 
--- scripts/tasks/build-javascript.js
+++ scripts/tasks/build-javascript.js
@@ -3,7 +3,7 @@ var glob = require('glob');
 var gulp = require('gulp');
 var browserify = require('browserify');
 
-function buildJavascript () {
+function buildJavascript (settings) {
   var entryFiles = glob.sync(settings.javascript.entryFiles);
   browserify({ entries: entryFiles })
     .transform('babelify', { presets: ['es2015', 'react'] })
