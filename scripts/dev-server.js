6b8a1815077d5a0cfefee273c988378d15b00c88
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-29 15:33:07 -0700

    removed css watch in favour of webpack

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -1,12 +1,4 @@
 'use strict';
-var watchCss = require('./tasks/watch-css');
 var runWebpackDevServer = require('./tasks/run-webpack-dev-server');
 
-function dev () {
-  // Do a build before starting
-  // require('./build');
-  watchCss();
-  runWebpackDevServer();
-}
-
-dev();
+runWebpackDevServer();
