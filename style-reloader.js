735a98554cd25ffb329a498dc7388c23622d3c28
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-30 11:37:47 -0700

    style reloader

---
+++ style-reloader.js
@@ -0,0 +1,20 @@
+/**
+ * Style reloader *only use in Development*
+ * ---
+ * This script is connected to the Webpack Dev Server
+ * and reloads every css file if there is an update.
+ */
+window.addEventListener('message', function (e) {
+  if (e.data.search('webpackHotUpdate') === -1) return;
+  console.log('Reloading css', e.data);
+
+  var links = document.querySelectorAll('link');
+
+  for (var i = 0, item; link = links[i]; i++) {
+    if (link.href.indexOf('http') > -1) {
+      var linkHref = link.href;
+      link.href = 'about:blank';
+      link.href = linkHref;
+    }
+  }
+}, false);
