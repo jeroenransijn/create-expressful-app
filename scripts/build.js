dfd9a66550b959a2e97d5f046cb79918f8ff3739
Jeroen Ransijn <jssrdesign@gmail.com>
2016-08-19 16:14:17 -0700

    removed missing require

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,5 @@
 'use strict';
 var settings = require('../config/settings');
-var buildCss = require('./tasks/build-css');
 var buildJavascript = require('./tasks/build-javascript');
 
 function build () {
