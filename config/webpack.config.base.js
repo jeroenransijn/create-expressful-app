553b81b417b4ecbd94ae24db1135b570413e08ae
Jeroen Ransijn <jssrdesign@gmail.com>
2016-09-28 22:23:26 +0200

    added webpack.DefinePlugin for NODE_ENV

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -70,6 +70,9 @@ function configBase (options) {
       };
     },
     plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      }),
       new ExtractTextPlugin('[name]')
     ].concat(plugins)
   };
